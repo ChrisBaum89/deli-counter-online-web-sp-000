76b253913319a127e3d787e9b0998b305252afa3
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 01:13:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,20 @@
 # Write your code here.
 def line(katz_deli)
+  line_count = katz_deli.count
+  new_array = Array.new
+  if line_count == 0
+    puts "The line is currently empty."
+  else
+    new_array = katz_deli
+    index_count = 0
+    line_num = index_count + 1
+    new_array.each do |i|
+      new_array[index_count] = "#{line_num}. #{i}"
+    end 
+    output = new_array.join 
+    #puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+      
+  end
 
 end
 
