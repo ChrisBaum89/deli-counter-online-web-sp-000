872b3fb093c02e46421d176ade65d9545f2a93a7
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 01:03:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+
+end
+
+def take_a_number(katz_deli, name)
+  number = katz_deli.count + 1
+  puts "Welcome, #{name}. You are number #{number} in line
+end
+
+def now_serving(katz_deli)
+
+end
