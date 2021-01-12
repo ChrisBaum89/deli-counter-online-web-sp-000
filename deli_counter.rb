ab3ae2494e338cb2e929229f5d2cf3952c399776
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 01:22:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,4 @@
 # Write your code here.
-#katz_deli = ["Logan", "Avi", "Spencer"]
 def line(katz_deli)
   line_count = katz_deli.count
   new_array = Array.new
@@ -15,16 +14,13 @@ def line(katz_deli)
       index_count += 1
     end
     puts "The line is currently:#{new_array.join}"
-    #puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
-    #puts "#{new_array}"
-
   end
 end
-#line(katz_deli)
 
+#katz_deli = ["Logan", "Avi", "Spencer"]
 def take_a_number(katz_deli, name)
   number = katz_deli.count + 1
-  #puts "Welcome, #{name}. You are number #{number} in line."
+  puts "Welcome, #{name}. You are number #{number} in line."
 end
 
 def now_serving(katz_deli)
