bbbcf98050d76bb49638e8d7b51760a9dfc43f9e
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 01:38:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -35,7 +35,7 @@ def now_serving(katz_deli)
     puts "Currently serving #{katz_deli.first}"
     puts "#{katz_deli}"
     new_array = katz_deli.shift
-    puts "#{katz_deli.shift}"
+    puts "#{new_array}"
   end
 end
 now_serving(katz_deli)
