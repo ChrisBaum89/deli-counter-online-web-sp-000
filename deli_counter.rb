10304da4430777671b2d3e417f928af0b6a339f8
Chris Baum <christopher.baum@valpo.edu>
2021-01-12 01:25:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ end
 #katz_deli = [""]
 #name = "Ada"
 def take_a_number(katz_deli, name)
-  number = katz_deli.count
+  number = katz_deli.count + 1
   puts "Welcome, #{name}. You are number #{number} in line."
 end
 #take_a_number(katz_deli, name)
